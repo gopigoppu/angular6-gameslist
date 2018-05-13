af8423102a0d623b82163d787853b907f2bfa2b0
gopigoppu <gopi.learner93@gmail.com>
2018-05-13 18:03:52 +0530

    sorting and pagination add

--- src/app/components/games-list/games-list.component.ts
+++ src/app/components/games-list/games-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../shared/services/api/api.service';
 import { PageService } from '../../shared/services/page/page.service';
+import { OrderPipe } from 'ngx-order-pipe';
 
 @Component({
   selector: 'app-games-list',
@@ -11,17 +12,31 @@ export class GamesListComponent implements OnInit {
 
   gameListData: any;
   p = 1;
+  order = 'title';
+  reverse = false;
+  sortedCollection: any[];
 
-  constructor(private apiService: ApiService, private pageService: PageService) { }
+  constructor(private apiService: ApiService, private pageService: PageService, private orderPipe: OrderPipe) { }
 
   ngOnInit() {
     this.apiService.getGameList().subscribe((data: any) => {
       // console.log(data);
       this.gameListData = data;
+      this.sortedCollection = this.orderPipe.transform(this.gameListData, this.order);
       setTimeout(() => {
         this.pageService.setBodyBGHeight('board-container', 'search-container');
       }, 0);
     });
+
+
+  }
+
+  setOrder(value: string) {
+    this.reverse = !this.reverse;
+    console.log(value);
+    setTimeout(() => {
+      this.pageService.setBodyBGHeight('board-container', 'search-container');
+    }, 0);
   }
 
 
